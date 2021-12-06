a56cb286472489c7ab54d526ace7757c589eecd3
Mauricio Lomeli <46548793+mjlomeli@users.noreply.github.com>
2021-12-06 10:31:18 -0800

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://Quickly-healthy:$FnBt7u7gxUmX-G@users.sel6a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: 'healhty-quickly'
-            //Make sure this is your own unique string
-}
