652b5daa6a48b22c8a41afcf48b3a3335008c519
Emmett He <emmetthe26@gmail.com>
2021-12-07 15:20:30 -0800

    add header title and background

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -4,17 +4,22 @@ import './splash.css';
 const Splash = () => {
   return (
     <div className="splash-container">
+
       <div className="header-container">
-        <h1 className="title">Making Health Care Easy</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-          in culpa qui officia deserunt mollit anim id est laborum
-        </p>
+        <div className="header-text">
+          <h1 className="header-title">Making Health Care Easy</h1>
+          <p className="header-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+        <img src="https://thevideoink.com/wp-content/uploads/2021/11/wysiwyg-uploads_1569586526901-doctor.jpg" className="header-bg" />
       </div>
+
       <div className="splash-service-container">
-        <img className="splash-service-bg" src="https://preview.colorlib.com/theme/medcare/img/banner/pattern_bg.jpg.webp" />
+        {/* <img className="splash-service-bg" src="https://preview.colorlib.com/theme/medcare/img/banner/pattern_bg.jpg.webp" /> */}
         <div className="splash-service">
           <div className="splash-component">
             <div className="splash-image-container-doctor splash-image-container">
@@ -41,7 +46,7 @@ const Splash = () => {
             <div className="splash-image-container-record splash-image-container">
               <img src="https://github.com/lyhourlay1/Quickly-healthy/blob/main/assets/splash/upload.png?raw=true" />
             </div>
-            <div className="splash-text">Upload health records for easy provider access</div>
+            <div className="splash-text">Upload health records</div>
           </div>
         </div>
       </div>
