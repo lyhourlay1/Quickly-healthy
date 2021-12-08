4132e288ced98f0c5502438db155fd11bd6e9cdf
grayson-poon <grayson.a.poon@gmail.com>
2021-12-07 22:02:49 -0800

    add more doctors to seed

--- seed.js
+++ seed.js
@@ -88,6 +88,86 @@ const doctors = [
       Wednesday: { start: 9, end: 13 },
     },
   }),
+  new Doctor({
+    name: "doctor6",
+    address: "6480 Weathers Pl #106, San Diego, CA 92121",
+    location: [-122.38753727150544, 37.76988455204271],
+    specialty: ["family doctor"],
+    insurances: [
+      "Aetna Life Insurance Company",
+      "American National Insurance Company",
+      "Kaiser Permanente Insurance Company",
+    ],
+    availabilites: {
+      Monday: { start: 8, end: 11 },
+      Tuesday: { start: 14, end: 17 },
+      Wednesday: { start: 9, end: 13 },
+    },
+  }),
+  new Doctor({
+    name: "doctor7",
+    address: "6480 Weathers Pl #106, San Diego, CA 92121",
+    location: [-122.40604602461732, 37.80651746619424],
+    specialty: ["family doctor"],
+    insurances: [
+      "Aetna Life Insurance Company",
+      "American National Insurance Company",
+      "Kaiser Permanente Insurance Company",
+    ],
+    availabilites: {
+      Monday: { start: 8, end: 11 },
+      Tuesday: { start: 14, end: 17 },
+      Wednesday: { start: 9, end: 13 },
+    },
+  }),
+  new Doctor({
+    name: "doctor8",
+    address: "6480 Weathers Pl #106, San Diego, CA 92121",
+    location: [-122.48839155195866, 37.76905339221309],
+    specialty: ["family doctor"],
+    insurances: [
+      "Aetna Life Insurance Company",
+      "American National Insurance Company",
+      "Kaiser Permanente Insurance Company",
+    ],
+    availabilites: {
+      Monday: { start: 8, end: 11 },
+      Tuesday: { start: 14, end: 17 },
+      Wednesday: { start: 9, end: 13 },
+    },
+  }),
+  new Doctor({
+    name: "doctor9",
+    address: "6480 Weathers Pl #106, San Diego, CA 92121",
+    location: [-122.40825780661905, 37.794305657183145],
+    specialty: ["family doctor"],
+    insurances: [
+      "Aetna Life Insurance Company",
+      "American National Insurance Company",
+      "Kaiser Permanente Insurance Company",
+    ],
+    availabilites: {
+      Monday: { start: 8, end: 11 },
+      Tuesday: { start: 14, end: 17 },
+      Wednesday: { start: 9, end: 13 },
+    },
+  }),
+  new Doctor({
+    name: "doctor10",
+    address: "6480 Weathers Pl #106, San Diego, CA 92121",
+    location: [-122.47351735088884, 37.75713773155823],
+    specialty: ["family doctor"],
+    insurances: [
+      "Aetna Life Insurance Company",
+      "American National Insurance Company",
+      "Kaiser Permanente Insurance Company",
+    ],
+    availabilites: {
+      Monday: { start: 8, end: 11 },
+      Tuesday: { start: 14, end: 17 },
+      Wednesday: { start: 9, end: 13 },
+    },
+  }),
 ];
 
 // connect mongoose
