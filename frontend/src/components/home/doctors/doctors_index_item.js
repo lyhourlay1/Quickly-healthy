8fc498637094d435b0e12ae00bbc7ddb372ca56e
grayson-poon <grayson.a.poon@gmail.com>
2021-12-07 22:45:30 -0800

    layout html for doctor index item

--- frontend/src/components/home/doctors/doctors_index_item.js
+++ frontend/src/components/home/doctors/doctors_index_item.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 import { DEFAULT_PROFILE_PICTURE } from "../../../util/icons_and_images_util";
 
 const DoctorIndexItem = ({ doctor }) => {
-  debugger
   return (
     <li className="doctor-index-item">
       <div className="item-header">
         <Link to={`/doctors/${doctor._id}`} className="doctor-image">
-          <img src={DEFAULT_PROFILE_PICTURE} />
+          <img src={DEFAULT_PROFILE_PICTURE} alt="" />
         </Link>
 
         <div className="doctor-titles">
           <Link to={`/doctors/${doctor._id}`}>{doctor.name}</Link>
           <div>{doctor.specialty}</div>
+          <div>{doctor.address}</div>
         </div>
       </div>
 
