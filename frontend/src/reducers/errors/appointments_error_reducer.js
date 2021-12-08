cedce228c68accf7a368bf3cdc921719f48a0753
mjlomeli <mjlomeli@uci.edu>
2021-12-08 12:10:53 -0800

    merged errors reducer

--- frontend/src/reducers/errors/appointments_error_reducer.js
+++ frontend/src/reducers/errors/appointments_error_reducer.js
@@ -1,13 +1,10 @@
 import {
     RECEIVE_APPOINTMENT,
-    RECEIVE_APPOINTMENT_ERROR
 } from "../../actions/appointment_actions";
 
 export default function appointmentErrorsReducer(prevState = [], action){
     Object.freeze(prevState);
     switch (action.type) {
-        case RECEIVE_APPOINTMENT_ERROR:
-            return [action.error];
         case RECEIVE_APPOINTMENT:
             return [];
         default:
