c9d229c38990ba6a10a097397b2bef5c2937fc65
grayson-poon <grayson.a.poon@gmail.com>
2021-12-07 22:40:00 -0800

    fix doctor schema

--- frontend/src/components/home/doctors/doctors_index_item.js
+++ frontend/src/components/home/doctors/doctors_index_item.js
@@ -1,15 +1,30 @@
 import React from "react"
+import { Link } from "react-router-dom";
+import { DEFAULT_PROFILE_PICTURE } from "../../../util/icons_and_images_util";
 
 const DoctorIndexItem = ({ doctor }) => {
+  debugger
   return (
     <li className="doctor-index-item">
       <div className="item-header">
-        <Link to={`/doctors/${doctor._id}`} className="item-header-image">
-          <img
-            src={}
-          />
+        <Link to={`/doctors/${doctor._id}`} className="doctor-image">
+          <img src={DEFAULT_PROFILE_PICTURE} />
         </Link>
+
+        <div className="doctor-titles">
+          <Link to={`/doctors/${doctor._id}`}>{doctor.name}</Link>
+          <div>{doctor.specialty}</div>
+        </div>
+      </div>
+
+      <div className="doctor-insurances">
+        <div>Accepted insuraces:</div>
+        {doctor.insurances.map((insurance, idx) => (
+          <div key={idx} id="insurance">{insurance}</div>
+        ))}
       </div>
     </li>
   );
-}
+};
+
+export default DoctorIndexItem;
