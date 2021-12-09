2cbdc12b90a4d41519413c214f6f644f2851f3d6
mjlomeli <mjlomeli@uci.edu>
2021-12-09 15:10:30 -0800

    removed lee's functions

--- frontend/src/actions/appointment_actions.js
+++ frontend/src/actions/appointment_actions.js
@@ -1,27 +1,80 @@
-import * as ApiAppointmentUtil from "../util/appointment_util";
+import * as AppointmentUtil from '../util/appointment_util'
+
+export const RECEIVE_APPOINTMENT = "RECEIVE_APPOINTMENT";
+export const RECEIVE_APPOINTMENTS = "RECEIVE_APPOINTMENTS";
+export const REMOVE_APPOINTMENT = "REMOVE_APPOINTMENT";
+export const RECEIVE_APPOINTMENT_ERROR = "RECEIVE_APPOINTMENT_ERROR";
 
 
-export const RECEIVE_APPOINTMENT = 'RECEIVE_APPOINTMENT'
-export const RECEIVE_APPOINTMENTS = 'RECEIVE_APPOINTMENTS'
 
 const receiveAppointments = appointments =>({
     type: RECEIVE_APPOINTMENTS,
-    appointments,
+    appointments: appointments
 })
 
-const receiveAppointment = appointment => ({
+const receiveAppointment = appointment =>({
     type: RECEIVE_APPOINTMENT,
-    appointment,
+    appointment: appointment
 })
 
-export const fetchUserAppointments = (userId) =>dispatch=> (
-    ApiAppointmentUtil.fetchUserAppointments(userId).then((appointments)=> dispatch(receiveAppointments(appointments)))
+const receiveAppointmentError = error =>({
+    type: RECEIVE_APPOINTMENT_ERROR,
+    error: error
+})
+
+const removeAppointment = id =>({
+    type: REMOVE_APPOINTMENT,
+    id: id
+})
+
+
+
+/*    Separation      */
+
+
+export const fetchAppointments = () => dispatch =>(
+    AppointmentUtil.fetchAppointments().then(
+        appointments => {
+            return dispatch(receiveAppointments(appointments.data))
+        },
+        err => dispatch(receiveAppointmentError(err.responseJSON))
+    )
 )
 
-export const fetchAppointment = (appointmentId) => dispatch=> (
-    ApiAppointmentUtil.fetchAppointment(appointmentId).then((appointment=> dispatch(receiveAppointment(appointment))))
+
+export const fetchAppointment = appointmentId => (dispatch) => {
+    return AppointmentUtil.fetchAppointment(appointmentId).then(
+        appointment => dispatch(receiveAppointment(appointment.data)),
+        err => dispatch(receiveAppointmentError(err.responseJSON))
+    )
+}
+
+
+export const createAppointment = appointment => dispatch =>(
+    AppointmentUtil.createAppointment(appointment).then(
+        appointment => dispatch(receiveAppointment(appointment.data)),
+        err => dispatch(receiveAppointmentError(err.responseJSON))
+    )
 )
 
-export const createAppointment = (appointment) => dispatch => (
-    ApiAppointmentUtil.createAppointment(appointment).then((appointment=> dispatch(receiveAppointment(appointment))))
+export const updateAppointment = appointment => dispatch =>(
+    AppointmentUtil.updateAppointment(appointment).then(
+        appointment => dispatch(receiveAppointment(appointment.data)),
+        err => dispatch(receiveAppointmentError(err.responseJSON))
+    )
 )
+
+export const deleteAppointment = appointmentId => dispatch =>(
+    AppointmentUtil.deleteAppointment(appointmentId).then(
+        appointment => dispatch(removeAppointment(appointment.data._id)),
+        err => dispatch(receiveAppointmentError(err.responseJSON))
+    )
+)
+
+
+// window.fetchAppointments = fetchAppointments;
+// window.fetchAppointment = fetchAppointment;
+// window.fetchUserAppointments = fetchUserAppointments;
+// window.createAppointment = createAppointment;
+// window.updateAppointment = updateAppointment;
+// window.deleteAppointment = deleteAppointment;
--- frontend/src/reducers/entities/appointments_reducer.js
+++ frontend/src/reducers/entities/appointments_reducer.js
@@ -1,15 +1,25 @@
-import { RECEIVE_APPOINTMENT, RECEIVE_APPOINTMENTS } from "../../actions/appointment_actions"
+import {
+    RECEIVE_APPOINTMENTS,
+    RECEIVE_APPOINTMENT,
+    REMOVE_APPOINTMENT
+} from "../../actions/appointment_actions";
 
-const AppointmentsReducer = (state={}, action)=>{
-    let newState = Object.assign({}, state)
+export default function AppointmentReducer(prevState={}, action){
+    Object.freeze(prevState);
+    let newState = Object.assign({}, prevState) // this isn't a deep copy
     switch(action.type){
         case RECEIVE_APPOINTMENTS:
-            return action.appointments
+            let appointment = Object.fromEntries(action.appointments.map(appointment => {
+                return [appointment._id, appointment]
+            }))
+            return {...newState, ...appointment};
         case RECEIVE_APPOINTMENT:
-            newState[action.appointment.id]= action.appointment
-            return newState
+            newState[action.appointment._id] = action.appointment;
+            return newState;
+        case REMOVE_APPOINTMENT:
+            delete newState[action.id]
+            return newState;
         default:
-            return state
+            return newState
     }
 }
-export default AppointmentsReducer
