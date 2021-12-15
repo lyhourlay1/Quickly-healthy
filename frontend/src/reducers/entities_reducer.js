59f70837100d208b1e0da5744fd4e848023a2b61
mjlomeli <mjlomeli@uci.edu>
2021-12-14 17:14:49 -0800

    changed users login to include user information

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -3,13 +3,11 @@ import { combineReducers } from "redux";
 import DoctorReducer from "./entities/doctors_reducer"
 import RecordReducer from "./entities/records_reducer"
 import AppointmentsReducer from "./entities/appointments_reducer"
-import UserReducer from "./entities/users_reducer";
 
 const entitiesReducer = combineReducers({
     doctor: DoctorReducer,
     record: RecordReducer,
-    appointments: AppointmentsReducer,
-    user: UserReducer
+    appointments: AppointmentsReducer
 });
 
 export default entitiesReducer;
