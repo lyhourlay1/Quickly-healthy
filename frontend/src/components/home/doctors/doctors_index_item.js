8f47ced78e1d4403cc8c4bd11448e7cd9ec5c3c5
Emmett He <emmetthe26@gmail.com>
2021-12-27 10:42:49 -0800

    changed doctors index to links

--- frontend/src/components/home/doctors/doctors_index_item.js
+++ frontend/src/components/home/doctors/doctors_index_item.js
@@ -1,41 +1,39 @@
-import React from "react"
-import { Link } from "react-router-dom";
-import { DEFAULT_PROFILE_PICTURE } from "../../../util/icons_and_images_util";
-import './doctors_index.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { DEFAULT_PROFILE_PICTURE } from '../../../util/icons_and_images_util';
+import './doctors_index.css';
 
 const DoctorIndexItem = ({ doctor }) => {
   return (
-    <li className="doctors-index-item">
-      <div className="item-header">
-        <Link className="doctors-image" to={`/doctors/${doctor._id}`}>
-          <div>
-            <img
-              src={
-                doctor.image
-                  ? `data:${doctor.image.mimetype};base64,${doctor.image.data}`
-                  : DEFAULT_PROFILE_PICTURE
-              }
-              alt="picture"
-            />
-          </div>
-        </Link>
-    
-        <div className="doctors-titles">
-          <Link to={`/doctors/${doctor._id}`}>{doctor.name}</Link>
-          <div>{doctor.specialty}</div>
-          <div id="address">{doctor.address}</div>
-        </div>
-      </div>
+    <Link to={`/doctors/${doctor._id}`}>
+      <li className="doctors-index-item">
+        <div className="item-header">
+          <Link className="doctors-image" to={`/doctors/${doctor._id}`}>
+            <div>
+              <img
+                src={doctor.image ? `data:${doctor.image.mimetype};base64,${doctor.image.data}` : DEFAULT_PROFILE_PICTURE}
+                alt="picture"
+              />
+            </div>
+          </Link>
 
-      <div className="doctors-insurances">
-        <div>In-network Insurance Providers:</div>
-        {doctor.insurances.map((insurance, idx) => (
-          <div key={idx} id="insurance">
-            {insurance}
+          <div className="doctors-titles">
+            <p className="doctors-titles-name">{doctor.name}</p>
+            <div>{doctor.specialty}</div>
+            <div id="address">{doctor.address}</div>
           </div>
-        ))}
-      </div>
-    </li>
+        </div>
+
+        <div className="doctors-insurances">
+          <div>In-network Insurance Providers:</div>
+          {doctor.insurances.map((insurance, idx) => (
+            <div key={idx} id="insurance">
+              {insurance}
+            </div>
+          ))}
+        </div>
+      </li>
+    </Link>
   );
 };
 
