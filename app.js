c7f8b8698dd9f4b947c4f796b885d6399e647d9b
Lyhour Lay <lyhourlay1@gmail.com>
2023-09-17 22:40:24 -0700

    change port

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.use('/api/records', records);
 app.use('/api/doctors', doctors);
 // app.use("/seed");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.listen(port);
