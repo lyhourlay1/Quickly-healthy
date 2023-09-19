22f1c7b8db6cd77f4f0a340fce8eb80a14883e45
Lyhour Lay <lyhourlay1@gmail.com>
2023-09-18 21:41:18 -0700

    set header

--- app.js
+++ app.js
@@ -9,13 +9,35 @@ var cors = require('cors')
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    // res.setHeader("Access-Control-Allow-Origin", "true");
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  // app.use(express.static("frontend/build"));
+  // app.get("/", (req, res) => {
+  //   // res.setHeader("Access-Control-Allow-Credentials", "true");
+  //   // res.setHeader("Access-Control-Allow-Origin", "true");
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  // });
+  app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 }
+
+
 
 app.use(fileUpload({
   createParentPath: true
