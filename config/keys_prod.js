5eb600f0fbeb298cf7f17eea5e76157120874313
Lyhour Lay <lyhourlay1@gmail.com>
2023-09-18 18:43:27 -0700

    modify key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,11 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY,
+//   reactAppMapboxToken: process.env.REACT_APP_MAPBOX_TOKEN
+// }
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  reactAppMapboxToken: process.env.REACT_APP_MAPBOX_TOKEN
-}
+  mongoURI:
+    "mongodb+srv://Quickly-healthy:$FnBt7u7gxUmX-G@users.sel6a.mongodb.net/Quickly-healthy?retryWrites=true&w=majority",
+  secretOrKey: "healhty-quickly",
+  reactAppMapboxToken: "pk.eyJ1IjoiZ3JheXNvbi1wb29uIiwiYSI6ImNrd3Zhbm4yeTFqaDkydm53N2tybTluamgifQ.Tm2VzXWmlCo1uHSB9k5jsg",
+};
